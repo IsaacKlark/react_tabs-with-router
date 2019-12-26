fa6cfc456ef5ad3141b8938f84f71ccc19d274f5
IsaacKlark <isaackeklarke@gmail.com>
2019-12-26 14:15:06 +0200

    Updates

---
+++ precache-manifest.9de5c81fbf6ed003f0532fee70d70fdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5bf43abcda4033265ac8532c0f57f5e",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "60dff99294b334b227d6",
+    "url": "/react_tabs-with-router/static/css/main.f44d06bd.chunk.css"
+  },
+  {
+    "revision": "a6ed9298720ddeba0193",
+    "url": "/react_tabs-with-router/static/js/2.605c7544.chunk.js"
+  },
+  {
+    "revision": "60dff99294b334b227d6",
+    "url": "/react_tabs-with-router/static/js/main.3f2d4222.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
